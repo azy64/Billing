3a7c521410a22afeb45b97332738fa7aa65978c6
cecethea <ui564839@rwe-ag.com>
2024-08-21 19:15:49 +0200

    add Login and Register controller

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +21,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class,[
+                "label"=>"Your email Address",
+                "required"=>true,
+                "attr"=>["class"=>"form-control"],
+                "row_attr"=>["class"=>"form-group m-1"]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,11 +35,31 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add("prenom",TextType::class,[
+                "attr"=>["class"=>"form-control"],
+                "required"=>true,
+                "label"=>"Your Firstname",
+                "row_attr"=>["class"=>"form-group m-1"]
+            ])
+            ->add("nom",TextType::class,[
+                "attr"=>["class"=>"form-control"],
+                "required"=>true,
+                "label"=>"Your Name",
+                "row_attr"=>["class"=>"form-group m-1"]
+            ])
+            ->add("phoneNumber",TelType::class,[
+                "attr"=>["class"=>"form-control"],
+                "required"=>true,
+                "label"=>"Your Phone Number",
+                "row_attr"=>["class"=>"form-group m-1"]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', "class"=>"form-control"],
+                "required"=>true,
+                "row_attr"=>["class"=>"form-group m-1"],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -50,6 +79,10 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            "constraints"=> [new UniqueEntity([
+                "fields"=>["email"],
+                "message"=>"This email address exist already in the system! please choose another one"
+            ])]
         ]);
     }
 }
